b217a0a56c8239e5738c348ab0f12f2fee984a60
Nwsheats <nwsheats@gmail.com>
2022-10-25 22:39:33 -0500

    added initial inquirer questions and README generation formatting, needs fixing

--- index.js
+++ index.js
@@ -1,10 +1,108 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer 
+    .prompt([
+    {
+        type: 'input',
+        message: 'What is the title of your Project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What does your application do?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Please enter usage information',
+        name: 'usage',
+    },
+    {
+        type: 'checkbox',
+        message: 'What license are you using?',
+        name: 'license',
+        choices: ['MIT', 'Apache', 'GPL'],
+    },
+    {
+        type: 'input',
+        message: 'What are the contribution guidelines?',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'What are your testing instructions?',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your GitHub Username',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+])
+.then((answers) => {
+    const md = `
+    # ${answers.title}
+
+    ## Description
+
+    ${answers.description}
+
+    ## Table of Contents
+
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+
+    ## Installation
+
+    ${answers.installation}
+
+    ## Usage
+
+    ${answers.installation}
+
+    ## License
+
+    ${answers.license}
+
+    ## Contributing
+
+    ${answers.contributing}
+
+    ## Tests
+
+    ${answers.tests}
+
+    ## Questions
+
+    ${answers.github}:
+    https://github.com/${answers.github}/
+    
+    If you have additional questions, please reach out to me at ${answers.email}
+    
+    `
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+fs.writeFile('README.md', md, (err) =>
+  err ? console.error(err) : console.log('Success!')
+);
+});
 
 // TODO: Create a function to initialize app
 function init() {}
